4cbfa40cf606446af3056feca7ec24ed907491a3
DianaLeee <yeonylee94@gmail.com>
2020-05-05 23:49:29 +0900

    Updates

---
+++ precache-manifest.305d1fe6feb8ee1538d3fb1469b6c08f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe94263160da94219260a46cbf84be9",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "3526701310a129f92f5e",
+    "url": "/react-deploy-test/static/js/2.39c7dd06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy-test/static/js/2.39c7dd06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4e3aa58bb1c15e9d2c",
+    "url": "/react-deploy-test/static/js/main.1c3ac5d4.chunk.js"
+  },
+  {
+    "revision": "1035dc85f45320d81314",
+    "url": "/react-deploy-test/static/js/runtime-main.aaf7d827.js"
+  }
+]);
