70289664219b23c5b349ca116586a12cdb321ed5
DianaLeee <yeonylee94@gmail.com>
2020-05-06 00:38:47 +0900

    Add Switch route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Contact from "./routes/Contact";
@@ -9,10 +9,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <Navigation />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route component={() => <div>404 not found</div>} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={() => <div>404 not found</div>} />
+      </Switch>
     </BrowserRouter>
   );
 };
